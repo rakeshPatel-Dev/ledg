155ba3059f36d96654994c91891272ac35af6419
Rakesh Patel <rk5080976@gmail.com>
2026-08-11 21:18:03 +0545

    feat(brand): add app logo and create centralized logo control and add to sign in/ up pages

---
+++ apps/web/src/components/common/AppLogo.tsx
@@ -0,0 +1,11 @@
+
+
+const AppLogo = () => {
+  return (
+     <span className="flex size-16 items-center justify-center rounded-4xl bg-primary text-primary-foreground shadow-lg shadow-primary/30">
+          <img loading="lazy" src="/ledg-logo.svg" className="size-10" />
+        </span>
+  )
+}
+
+export default AppLogo
