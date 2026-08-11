9a46def9c1491c0a1dc4ee3606e513a8d685bc91
Rakesh Patel <rk5080976@gmail.com>
2026-08-11 21:19:50 +0545

    fix(AppLogo): add alt attribute to image for accessibility

--- apps/web/src/components/common/AppLogo.tsx
+++ apps/web/src/components/common/AppLogo.tsx
@@ -3,7 +3,7 @@
 const AppLogo = () => {
   return (
      <span className="flex size-16 items-center justify-center rounded-4xl bg-primary text-primary-foreground shadow-lg shadow-primary/30">
-          <img loading="lazy" src="/ledg-logo.svg" className="size-10" />
+          <img loading="lazy"  alt="Ledg logo" src="/ledg-logo.svg" className="size-10" />
         </span>
   )
 }
